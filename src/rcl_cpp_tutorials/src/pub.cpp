95395381122ff6a8a1197ddb2cd159c66a66d2fb
李茂 <limao1@xiaomi.com>
2021-05-21 15:36:43 +0800

    paramter usage

--- src/rcl_cpp_tutorials/src/pub.cpp
+++ src/rcl_cpp_tutorials/src/pub.cpp
@@ -16,6 +16,24 @@ public:
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(100ms, std::bind(&MinimalPublisher::timer_callback, this));
+    node_name_ = "my_publisher";
+    if (!this->has_parameter("age"))
+    {
+      this->declare_parameter("age", rclcpp::ParameterValue(250));
+    }
+
+    // 声明就建立参数， 并带有参数
+
+    if (this->has_parameter("age"))
+    {
+      int age = 0;
+
+      bool ret = this->get_parameter("age", age);
+      std::cout << "result " << (int)ret << " age " << age << std::endl;
+    }
+    // nav2 中先声明， 后get，可能是结合参数文件yaml的方式，加载yaml后覆盖参数值
+    // declare_parameter_if_not_declared
+    // node->get_parameter()
   }
 
 private:
@@ -30,6 +48,7 @@ private:
   size_t count_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
+  std::string node_name_;
 };
 
 int main(int argc, char** argv)
